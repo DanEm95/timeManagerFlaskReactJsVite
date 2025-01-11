a5ce9d2f448956b75e2f4fcac335a2f5e6c2e3f0
DanEm95 <110663668+DanEm95@users.noreply.github.com>
2025-01-11 17:05:46 +0100

    added env to store secret_key

--- server/app.py
+++ server/app.py
@@ -2,24 +2,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 CORS(app)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ultraSecretAndSecure'	
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.environ.get("secret_key")
 
 
 db = SQLAlchemy(app)
-
-
-# Configure Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
 import routes
@@ -29,5 +24,5 @@ with app.app_context():
     db.create_all()
 
 
-if (__name__ == '__main__'):
+if __name__ == "__main__":
     app.run(debug=True)
